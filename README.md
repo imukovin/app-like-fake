6ff84bd3a4042143ae20a1d0d1420ccec79ecc4a
a.mukovin <a.mukovin@intabia.ru>
2023-05-26 22:50:59 +0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# App For Fun
+
+Just to try a new framework.
+
+#### [Compose Multiplatform](https://github.com/JetBrains/compose-multiplatform) mobile application
---
+++ shared/src/commonMain/kotlin/App.kt
@@ -0,0 +1,12 @@
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun App() {
+    MaterialTheme {
+
+        Text("Test")
+
+    }
+}
